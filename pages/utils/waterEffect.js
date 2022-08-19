d7bacbe316c98452519bd59a814b888f7dde4b97
keshiamukenge <mukenge.keshia@gmail.com>
2022-08-19 16:02:40 +0200

    feat: add rgb water effect

--- pages/utils/waterEffect.js
+++ pages/utils/waterEffect.js
@@ -1,3 +1,14 @@
+import * as THREE from 'three'
+
+const easeOutSine = (t, b, c, d) => {
+  return c * Math.sin((t / d) * (Math.PI / 2)) + b;
+};
+
+const easeOutQuad = (t, b, c, d) => {
+  t /= d;
+  return -c * t * (t - 2) + b;
+};
+
 const size = window.innerWidth
 
 const wavesOptions = {
@@ -8,6 +19,7 @@ const wavesOptions = {
 	width: size,
 	height: size,
 	last: null,
+	texture: null,
 }
 
 export function initTexture({ width, height }) {
@@ -18,6 +30,8 @@ export function initTexture({ width, height }) {
 	canvas.height = height;
 	const ctx = canvas.getContext("2d");
 	clear({ ctx, canvas });
+
+	wavesOptions.texture = new THREE.Texture(canvas);
 }
 
 export function clear({ ctx, canvas }) {
@@ -81,6 +95,8 @@ export function updatePoints() {
 	wavesOptions.points.forEach(point => {
     drawPoint(point);
   });
+
+	wavesOptions.texture.needsUpdate = true;
 }
 
 export function drawPoint(point) {
@@ -90,13 +106,26 @@ export function drawPoint(point) {
 		y: point.y * wavesOptions.height
 	};
 	const radius = wavesOptions.radius;
-
-	let intensity = 1;
-	intensity = 1 - point.age / wavesOptions.maxAge;
-
 	const ctx = document.querySelector('canvas#WaterTexture').getContext("2d")
 
-	const color = "255,255,255";
+	let intensity = 1;
+	if (point.age < wavesOptions.maxAge * 0.3) {
+		intensity = easeOutSine(point.age / (wavesOptions.maxAge * 0.3), 0, 1, 1);
+	} else {
+		intensity = easeOutQuad(
+			1 - (point.age - wavesOptions.maxAge * 0.3) / (wavesOptions.maxAge * 0.7),
+			0,
+			1,
+			1
+		);
+	}
+	intensity *= point.force;
+
+	const red = ((point.vx + 1) / 2) * 255;
+	const green = ((point.vy + 1) / 2) * 255;
+	// B = Unit vector
+	const blue = intensity * 255;
+	const color = `${red}, ${green}, ${blue}`;
 
 	const offset = wavesOptions.width * 5;
 	// 1. Give the shadow a high offset.
@@ -110,6 +139,43 @@ export function drawPoint(point) {
 	// 2. Move the circle to the other direction of the offset
 	ctx.arc(position.x - offset, position.y - offset, radius, 0, Math.PI * 2);
 	ctx.fill();
+
+
+
+	// const position = {
+	// 	x: point.x * wavesOptions.width,
+	// 	y: point.y * wavesOptions.height
+	// };
+	// const radius = wavesOptions.radius;
+	// const ctx = document.querySelector('canvas#WaterTexture').getContext("2d")
+
+	// let intensity = 1;
+	// if (point.age < wavesOptions.maxAge * 0.3) {
+	// 	intensity = easeOutSine(point.age / (wavesOptions.maxAge * 0.3), 0, 1, 1);
+	// } else {
+	// 	intensity = easeOutQuad(
+	// 		1 - (point.age - wavesOptions.maxAge * 0.3) / (wavesOptions.maxAge * 0.7),
+	// 		0,
+	// 		1,
+	// 		1
+	// 	);
+	// }
+	// intensity *= point.force;
+
+	// const color = "255,255,255";
+
+	// const offset = wavesOptions.width * 5;
+	// // 1. Give the shadow a high offset.
+	// ctx.shadowOffsetX = offset;
+	// ctx.shadowOffsetY = offset;
+	// ctx.shadowBlur = radius * 1;
+	// ctx.shadowColor = `rgba(${color},${0.2 * intensity})`;
+
+	// ctx.beginPath();
+	// ctx.fillStyle = "rgba(255,0,0,1)";
+	// // 2. Move the circle to the other direction of the offset
+	// ctx.arc(position.x - offset, position.y - offset, radius, 0, Math.PI * 2);
+	// ctx.fill();
 }
 
 export default { initTexture, clear, addPoint, updatePoints, drawPoint };
